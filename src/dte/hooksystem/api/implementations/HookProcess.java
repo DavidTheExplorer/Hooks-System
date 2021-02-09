43cc6eff12878469010539a63bba162680e01b82
David <darknight4123@gmail.com>
2021-02-09 07:17:15 +0200

    Uploaded the API

---
+++ src/dte/hooksystem/api/implementations/HookProcess.java
@@ -0,0 +1,54 @@
+package dte.hooksystem.api.implementations;
+
+import static dte.hooksystem.plugins.absencehandlers.factory.AbsenceHandlersFactory.disablePlugin;
+
+import java.util.Objects;
+
+import dte.hooksystem.hooks.PluginHook;
+import dte.hooksystem.hooks.service.IHookService;
+import dte.hooksystem.plugins.absencehandlers.PluginAbsenceHandler;
+import dte.hooksystem.plugins.absencehandlers.factory.AbsenceHandlersFactory;
+
+//TODO: Add in the dosc that although hooks are required by default, It's recommended to call the required() method to avoid unexpected behavior.
+//And that a required hook means the plugin will disable itself if the hook is not present.
+public class HookProcess
+{
+	private final IHookService hookService;
+	
+	private boolean required = true;
+	private PluginAbsenceHandler pluginAbsenceHandler;
+	
+	public HookProcess(IHookService hookService) 
+	{
+		this.hookService = hookService;
+	}
+	public HookProcess required() 
+	{
+		this.required = true;
+		return this;
+	}
+	public HookProcess softdepend()
+	{
+		this.required = false;
+		return this;
+	}
+	public HookProcess ifPluginAbsent(PluginAbsenceHandler handler)
+	{
+		Objects.requireNonNull(handler);
+		
+		if(this.required)
+			//if the hooks are required, the provided handler will run before before the plugin is disabled
+			this.pluginAbsenceHandler = AbsenceHandlersFactory.handleOrdered(handler, disablePlugin(this.hookService.getOwningPlugin()));
+		else
+			this.pluginAbsenceHandler = handler;
+
+		return this;
+	}
+	public void hookTo(PluginHook... hooksToRegister)
+	{
+		Objects.requireNonNull(hooksToRegister);
+
+		for(PluginHook hook : hooksToRegister) 
+			this.hookService.hookTo(hook, this.pluginAbsenceHandler);
+	}
+}
---
+++ src/dte/hooksystem/api/implementations/HookService.java
@@ -0,0 +1,97 @@
+package dte.hooksystem.api.implementations;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Consumer;
+
+import org.bukkit.plugin.Plugin;
+
+import dte.hooksystem.exceptions.HookInitException;
+import dte.hooksystem.exceptions.PluginAlreadyHookedException;
+import dte.hooksystem.hooks.PluginHook;
+import dte.hooksystem.hooks.repository.IHookRepository;
+import dte.hooksystem.hooks.service.IHookService;
+import dte.hooksystem.plugins.absencehandlers.PluginAbsenceHandler;
+
+public class HookService implements IHookService
+{
+	private final Plugin owningPlugin;
+	private final IHookRepository hookRepository;
+
+	public HookService(Plugin owningPlugin, IHookRepository hookRepository)
+	{
+		this.owningPlugin = Objects.requireNonNull(owningPlugin);
+		this.hookRepository = Objects.requireNonNull(hookRepository);
+	}
+	
+	@Override
+	public void hookTo(PluginHook hook, PluginAbsenceHandler pluginAbsenceHandler) throws PluginAlreadyHookedException, HookInitException
+	{
+		Objects.requireNonNull(hook);
+
+		//if the hook's plugin is absent, call the handler and don't register the hook
+		Optional<Plugin> pluginHolder = hook.getPlugin();
+
+		if(!pluginHolder.isPresent())
+		{
+			pluginAbsenceHandler.handle(hook);
+			return;
+		}
+		Plugin plugin = pluginHolder.get();
+
+		//a plugin can't have 2 different hooks
+		if(this.hookRepository.isHooked(plugin)) 
+			throw new PluginAlreadyHookedException(plugin);
+
+		//init the hook
+		try
+		{
+			hook.init();
+		}
+		catch(Exception exception)
+		{
+			throw new HookInitException(hook.getPluginName(), exception);
+		}
+
+		//Register the hook
+		this.hookRepository.register(hook);
+	}
+
+	@Override
+	public <H extends PluginHook> Optional<H> findHook(Class<H> hookClass)
+	{
+		return this.hookRepository.findHook(hookClass);
+	}
+
+	@Override
+	public <T> List<T> findHooksOf(Class<T> hookTypeClass)
+	{
+		return this.hookRepository.findHooksOf(hookTypeClass);
+	}
+
+	@Override
+	public <T> Optional<T> findHookOf(Class<T> hookTypeClass, Consumer<List<T>> conflictsHandler)
+	{
+		return this.hookRepository.findHookOf(hookTypeClass, conflictsHandler);
+	}
+	
+	@Override
+	public Plugin getOwningPlugin() 
+	{
+		return this.owningPlugin;
+	}
+
+	@Override
+	public Set<PluginHook> getHooksView()
+	{
+		return this.hookRepository.getHooksView();
+	}
+
+	@Override
+	public int hooksAmount() 
+	{
+		return this.hookRepository.size();
+	}
+}
