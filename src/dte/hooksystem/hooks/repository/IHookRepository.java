1279f51349f310636114888594fc498282f242a8
David <darknight4123@gmail.com>
2021-02-09 07:12:48 +0200

    Updated the hooks

---
+++ src/dte/hooksystem/hooks/repository/IHookRepository.java
@@ -0,0 +1,75 @@
+package dte.hooksystem.hooks.repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Consumer;
+
+import org.bukkit.plugin.Plugin;
+
+import dte.hooksystem.hooks.PluginHook;
+
+public interface IHookRepository
+{
+	/**
+	 * Registers a new hook which can be retrieved by its class.
+	 * <p>
+	 * A plugin can only have <b>one</b> hook registered for, so there is no need to specify the plugin's name in any method.
+	 * 
+	 * @param hook The hook to register.
+	 */
+	void register(PluginHook hook);
+
+	/**
+	 * Returns the registered instance of the given hook class. 
+	 * <p>
+	 * A plugin can only have <b>one</b> hook registered for, so there is no need to specify the plugin's name in any method.
+	 * 
+	 * @param <H> The type of the hook.
+	 * @param hookClass The class of the hook.
+	 * @return The registered hook of the given hook class.
+	 */
+	<H extends PluginHook> Optional<H> findHook(Class<H> hookClass);
+	
+	/**
+	 * Returns all the hooks that are <i>subtype</i> or <i>implement</i> the specified parent type.
+	 * 
+	 * @param <T> The parent type.
+	 * @param hookTypeClass The parent class.
+	 * @return A list of the hooks that extend the specified parent type.
+	 */
+	<T> List<T> findHooksOf(Class<T> hookTypeClass);
+	
+	/**
+	 * Returns the registered hook which is subtype of the provided {@code hook class}; 
+	 * If more than 1 was registered, the provided {@code conflictsHandler} is executed.
+	 * 
+	 * @param <T> The parent type.
+	 * @param hookTypeClass The type of the hook.
+	 * @param conflictsHandler What to do if 2 hooks of the provided {@code hook class} were registered, it accepts the list of the found hooks.
+	 * @return The registered hook of the provided {@code hook type}.
+	 */
+	<T> Optional<T> findHookOf(Class<T> hookTypeClass, Consumer<List<T>> conflictsHandler);
+	
+	/**
+	 * Checks whether this registry contains a hook that is hooked to the given plugin.
+	 * 
+	 * @param plugin The potentially hooked plugin.
+	 * @return Whether this registry has an hook for the given plugin.
+	 */
+	boolean isHooked(Plugin plugin);
+	
+	/**
+	 * Returns the amount of registered hooks in this registry.
+	 * 
+	 * @return The registerd hooks amount.
+	 */
+	int size();
+	
+	/**
+	 * Returns a view set of the currently registered hooks in this registry.
+	 * 
+	 * @return The current registered hooks.
+	 */
+	Set<PluginHook> getHooksView();
+}
---
+++ src/dte/hooksystem/hooks/service/IHookService.java
@@ -0,0 +1,35 @@
+package dte.hooksystem.hooks.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Consumer;
+
+import org.bukkit.plugin.Plugin;
+
+import dte.hooksystem.exceptions.HookInitException;
+import dte.hooksystem.exceptions.PluginAlreadyHookedException;
+import dte.hooksystem.hooks.PluginHook;
+import dte.hooksystem.plugins.absencehandlers.PluginAbsenceHandler;
+
+public interface IHookService
+{
+	Plugin getOwningPlugin();
+	
+	/**
+	 * Registers the provided {@code hook} as supported by the plugin that owns this service, and initializes it.
+	 * 
+	 * @param hook The hook to register.
+	 * @param handler The handler to run when a hook cannot be registered because its plugin is not on the server.
+	 * @throws PluginAlreadyHookedException If this service already has a registered hook for the provided hook's plugin.
+	 * @throws HookInitException If there was a problem during the hook's {@code init()} method.
+	 */
+	void hookTo(PluginHook hook, PluginAbsenceHandler handler) throws PluginAlreadyHookedException, HookInitException;
+	
+	<H extends PluginHook> Optional<H> findHook(Class<H> hookClass);
+	<T> List<T> findHooksOf(Class<T> hookTypeClass);
+	<T> Optional<T> findHookOf(Class<T> hookTypeClass, Consumer<List<T>> conflictsHandler);
+	
+	Set<PluginHook> getHooksView();
+	int hooksAmount();
+}
