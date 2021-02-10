b084ef7c9a0f8a97d91c979564d59a540b694f00
David <darknight4123@gmail.com>
2021-02-10 06:50:57 +0200

    Updated 2 util classes

--- src/dte/hooksystem/utils/ArrayUtils.java
+++ src/dte/hooksystem/utils/ArrayUtils.java
@@ -4,36 +4,16 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.function.Supplier;
 
+import com.google.common.collect.Lists;
+
 public class ArrayUtils
 {
 	//Container of static methods
 	private ArrayUtils(){}
-
-	@SuppressWarnings("unchecked")
-	public static <T> T[] addTo(T[] array, T... elements)
-	{
-		return merge(array, elements);
-	}
-
-	@SuppressWarnings("unchecked")
-	public static <T> T[] merge(T[] array1, T[] array2) 
-	{
-		T[] result = (T[]) new Object[array1.length + array2.length];
-		
-		//insert the first array's elements
-		for(int i = 0; i < array1.length; i++)
-			result[i] = array1[i];
-		
-		//insert the second array's elements
-		for(int i = 0; i < array2.length; i++) 
-			result[array1.length + i] = array2[i];
-		
-		return result;
-	}
 	
 	public static <T> Iterable<T> toIterable(T[] array)
 	{
-		return Arrays.asList(array);
+		return Lists.newArrayList(array);
 	}
 	public static <T, C extends Collection<T>> C toCollection(T[] array, Supplier<C> baseSupplier)
 	{
