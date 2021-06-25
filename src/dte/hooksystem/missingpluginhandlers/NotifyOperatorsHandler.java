9a63f59d852d5a25cbaadf8a68ef5ae6d1f2a05b
David <darknight4123@gmail.com>
2021-06-25 06:09:34 +0300

    Removed an unnecessary optimization from NotifyOperatorsHandler

--- src/dte/hooksystem/missingpluginhandlers/NotifyOperatorsHandler.java
+++ src/dte/hooksystem/missingpluginhandlers/NotifyOperatorsHandler.java
@@ -1,27 +1,13 @@
 package dte.hooksystem.missingpluginhandlers;
 
-import dte.hooksystem.hooks.PluginHook;
 import dte.hooksystem.missingpluginhandlers.messager.MessagerHandler;
 import dte.hooksystem.utils.OnlineOperators;
 
 public class NotifyOperatorsHandler extends MessagerHandler
 {
 	@Override
-	public void handle(PluginHook failedHook)
-	{
-		String[] messages = createMessagesFor(failedHook);
-		
-		notifyOperators(messages);
-	}
-	
-	@Override
 	protected void sendMessage(String message) 
 	{
-		notifyOperators(message);
-	}
-	
-	private void notifyOperators(String... messages) 
-	{
-		OnlineOperators.get().forEach(operator -> operator.sendMessage(messages));
+		OnlineOperators.get().forEach(operator -> operator.sendMessage(message));
 	}
 }
